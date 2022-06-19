eee0ef70188d0e487fa9cd4c6f45a82280b97c93
rodps <rodrigopds73@gmail.com>
2022-06-19 15:43:52 -0300

    test: ensure only value or only error can be assigned

--- src/domain/core/value-object.spec.ts
+++ src/domain/core/value-object.spec.ts
@@ -9,4 +9,8 @@ describe('Value Object', () => {
   test('Should throws if value and error are not assigneds', () => {
     expect(() => { new ValueObjectMock() }).toThrow('Value and error are not assigneds')
   })
+
+  test('Should throws if value and error are assigneds', () => {
+    expect(() => { new ValueObjectMock('any_value', 'any_error') }).toThrow('Only value or only error can be assigned')
+  })
 })
