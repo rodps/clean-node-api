af116f8170ab8159eefdbb85011a777340217b85
rodps <rodrigopds73@gmail.com>
2022-06-19 00:22:44 -0300

    test: ensure EntityID returns no error when valid UUID is provided

--- src/domain/core/entity-id.spec.ts
+++ src/domain/core/entity-id.spec.ts
@@ -1,7 +1,7 @@
 import { EntityID } from './entity-id'
 
 describe('Entity ID', () => {
-  test('Should return an instance with a valid UUID when no param is provided', () => {
+  test('Should return an instance with a valid UUID when no one is provided', () => {
     const { id, err } = EntityID.create()
     const validUUIDRegex = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/
     expect(err).toBeFalsy()
@@ -14,9 +14,10 @@ describe('Entity ID', () => {
     expect(err).toBe('Invalid UUID')
   })
 
-  test('Should return an instance with same UUID', () => {
+  test('Should return an instance with same valid UUID provided and no error', () => {
     const validUuid = '58abc676-8362-4145-9225-3016abdbb828'
-    const { id } = EntityID.create(validUuid)
+    const { id, err } = EntityID.create(validUuid)
     expect(id?.id).toBe(validUuid)
+    expect(err).toBeFalsy()
   })
 })
