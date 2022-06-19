200131730e1d5b786cd8258960921aec4cd2ae2f
rodps <rodrigopds73@gmail.com>
2022-06-19 16:06:09 -0300

    test: ensure constructor assign correct values to object properties

--- src/domain/core/value-object.spec.ts
+++ src/domain/core/value-object.spec.ts
@@ -13,4 +13,16 @@ describe('Value Object', () => {
   test('Should throws if value and error are assigneds', () => {
     expect(() => { new ValueObjectMock('any_value', 'any_error') }).toThrow('Only value or only error can be assigned')
   })
+
+  test('Should assign correct values to the object properties', () => {
+    const value = 'any_value'
+    const valueObjectMockWithValue = new ValueObjectMock(value)
+    expect(valueObjectMockWithValue.value).toBe(value)
+    expect(valueObjectMockWithValue.err).toBeFalsy()
+
+    const err = 'any_error'
+    const valueObjectMockWithError = new ValueObjectMock(undefined, err)
+    expect(valueObjectMockWithError.value).toBeFalsy()
+    expect(valueObjectMockWithError.err).toBe(err)
+  })
 })
