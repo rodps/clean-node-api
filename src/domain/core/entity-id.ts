2185c8ef865490ca8d10631422f2bde33bf57f88
rodps <rodrigopds73@gmail.com>
2022-06-18 23:59:08 -0300

    feat: add error validation to EntityID

--- src/domain/core/entity-id.ts
+++ src/domain/core/entity-id.ts
@@ -1,9 +1,21 @@
-import { v4 as uuid } from 'uuid'
+import { v4 as uuid, validate } from 'uuid'
+
+export interface EntityIDResult {
+  id?: EntityID
+  err?: string
+}
 
 export class EntityID {
   readonly id: string
 
-  constructor (id?: string) {
-    this.id = id ?? uuid()
+  private constructor (id: string) {
+    this.id = id
+  }
+
+  static create (id?: string): EntityIDResult {
+    if (id) {
+      return validate(id) ? { id: new EntityID(id) } : { err: 'Invalid UUID' }
+    }
+    return { id: new EntityID(uuid()) }
   }
 }
