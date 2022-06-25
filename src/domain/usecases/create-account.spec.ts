8c9750dac26185bc6870c66a6db85f06853af8d3
rodps <rodrigopds73@gmail.com>
2022-06-24 23:46:49 -0300

    test: ensure create account calls create user repo with correct values

--- src/domain/usecases/create-account.spec.ts
+++ src/domain/usecases/create-account.spec.ts
@@ -3,21 +3,23 @@ import { CreateUserRepository } from '../ports/repositories/create-user-reposito
 import { CreateAccount } from './create-account'
 
 class CreateUserRepositorySpy implements CreateUserRepository {
+  user: UserProps
   create (user: UserProps): boolean {
+    this.user = user
     return true
   }
 }
 
 interface SutTypes {
-  createUserRepo: CreateUserRepositorySpy
+  createUserRepositorySpy: CreateUserRepositorySpy
   sut: CreateAccount
 }
 
 const makeSut = (): SutTypes => {
-  const createUserRepo = new CreateUserRepositorySpy()
-  const sut = new CreateAccount(createUserRepo)
+  const createUserRepositorySpy = new CreateUserRepositorySpy()
+  const sut = new CreateAccount(createUserRepositorySpy)
   return {
-    createUserRepo,
+    createUserRepositorySpy,
     sut
   }
 }
@@ -40,9 +42,20 @@ describe('Create account', () => {
       email: 'any_email@mail.com',
       password: 'any_password'
     }
-    const { sut, createUserRepo } = makeSut()
-    jest.spyOn(createUserRepo, 'create').mockReturnValue(false)
+    const { sut, createUserRepositorySpy } = makeSut()
+    jest.spyOn(createUserRepositorySpy, 'create').mockReturnValue(false)
     const result = sut.exec(userProps)
     expect(result).toBe(false)
+  })
+
+  test('Should call create user repository with correct values', () => {
+    const userProps = {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    }
+    const { sut, createUserRepositorySpy } = makeSut()
+    sut.exec(userProps)
+    expect(createUserRepositorySpy.user).toEqual(userProps)
   })
 })
