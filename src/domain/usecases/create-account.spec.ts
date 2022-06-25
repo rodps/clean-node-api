0cd99e682d5b401b121f82d951deaec62b6532ac
rodps <rodrigopds73@gmail.com>
2022-06-24 23:34:10 -0300

    test: ensure create account returns false if repository returns false

--- src/domain/usecases/create-account.spec.ts
+++ src/domain/usecases/create-account.spec.ts
@@ -20,4 +20,17 @@ describe('Create account', () => {
     const result = createAccount.exec(userProps)
     expect(result).toBe(true)
   })
+
+  test('Should return false if user repository returns false', () => {
+    const userProps = {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    }
+    const createUserRepo = new CreateUserRepositorySpy()
+    jest.spyOn(createUserRepo, 'create').mockReturnValue(false)
+    const createAccount = new CreateAccount(createUserRepo)
+    const result = createAccount.exec(userProps)
+    expect(result).toBe(false)
+  })
 })
