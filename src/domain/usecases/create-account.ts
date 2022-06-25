7e951880929e7db627c946959e71dc300ab65057
rodps <rodrigopds73@gmail.com>
2022-06-24 23:28:49 -0300

    feat: add create account use case

---
+++ src/domain/usecases/create-account.ts
@@ -0,0 +1,10 @@
+import { UserProps } from '../entities/user'
+import { CreateUserRepository } from '../ports/repositories/create-user-repository'
+
+export class CreateAccount {
+  constructor (readonly createUserRepo: CreateUserRepository) {}
+
+  exec (user: UserProps): boolean {
+    return true
+  }
+}
