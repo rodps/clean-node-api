a7f7921e29a616ba83b6be182d558faa35a20523
rodps <rodrigopds73@gmail.com>
2022-06-27 22:08:28 -0300

    test: ensure authorize returns err if password is not correct

--- src/domain/usecases/authorize.spec.ts
+++ src/domain/usecases/authorize.spec.ts
@@ -1,12 +1,38 @@
 import { LoadUserByEmailRepositorySpy } from '@mocks/domain/ports/repositories/load-user-by-email-repository-spy'
 import { Authorize } from './authorize'
 
+interface SutTypes {
+  loadUserByEmailRepositorySpy: LoadUserByEmailRepositorySpy
+  sut: Authorize
+}
+
+const makeSut = (): SutTypes => {
+  const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+  const sut = new Authorize(loadUserByEmailRepositorySpy)
+  return {
+    loadUserByEmailRepositorySpy,
+    sut
+  }
+}
+
 describe('Authorize', () => {
   test('should return err if email dont exists', () => {
-    const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
-    const authorize = new Authorize(loadUserByEmailRepositorySpy)
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
     loadUserByEmailRepositorySpy.result = null
-    const { err } = authorize.exec('any_email', 'any_password')
+    const { err } = sut.exec('any_email', 'any_password')
     expect(err).toBe('Email not registered')
+  })
+
+  test('should should return err if password dont match', () => {
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+    loadUserByEmailRepositorySpy.result = {
+      id: 'any_id',
+      createdAt: new Date(),
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password'
+    }
+    const { err } = sut.exec('any_email', 'incorrect_password')
+    expect(err).toBe('Incorrect password')
   })
 })
