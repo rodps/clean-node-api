1dd1a89b57e777953257a49bca3d5558ebc6eee0
rodps <rodrigopds73@gmail.com>
2022-06-28 02:13:45 -0300

    test: ensure authorize return an access token when provided correct credentials

--- src/domain/usecases/authorize.spec.ts
+++ src/domain/usecases/authorize.spec.ts
@@ -1,16 +1,28 @@
+import { AccessTokenGeneratorSpy } from '@/../tests/mocks/domain/ports/crypt/access-token-generator-spy'
+import { PasswordHasherSpy } from '@/../tests/mocks/domain/ports/crypt/password-hasher-spy'
 import { LoadUserByEmailRepositorySpy } from '@mocks/domain/ports/repositories/load-user-by-email-repository-spy'
 import { Authorize } from './authorize'
 
 interface SutTypes {
   loadUserByEmailRepositorySpy: LoadUserByEmailRepositorySpy
+  accessTokenGeneratorSpy: AccessTokenGeneratorSpy
+  passwordHasherSpy: PasswordHasherSpy
   sut: Authorize
 }
 
 const makeSut = (): SutTypes => {
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
-  const sut = new Authorize(loadUserByEmailRepositorySpy)
+  const accessTokenGeneratorSpy = new AccessTokenGeneratorSpy()
+  const passwordHasherSpy = new PasswordHasherSpy()
+  const sut = new Authorize(
+    loadUserByEmailRepositorySpy,
+    accessTokenGeneratorSpy,
+    passwordHasherSpy
+  )
   return {
     loadUserByEmailRepositorySpy,
+    accessTokenGeneratorSpy,
+    passwordHasherSpy,
     sut
   }
 }
@@ -19,20 +31,43 @@ describe('Authorize', () => {
   test('should return err if email dont exists', () => {
     const { sut, loadUserByEmailRepositorySpy } = makeSut()
     loadUserByEmailRepositorySpy.result = null
-    const { err } = sut.exec('any_email', 'any_password')
+    const { token, err } = sut.exec('any_email', 'any_password')
+    expect(token).toBeFalsy()
     expect(err).toBe('Email not registered')
   })
 
-  test('should should return err if password dont match', () => {
-    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+  test('should return err if password is wrong', () => {
+    const { sut, loadUserByEmailRepositorySpy, passwordHasherSpy } = makeSut()
     loadUserByEmailRepositorySpy.result = {
       id: 'any_id',
       createdAt: new Date(),
       name: 'any_name',
       email: 'any_email',
-      password: 'any_password'
+      password: 'hashed_password'
     }
-    const { err } = sut.exec('any_email', 'incorrect_password')
+    passwordHasherSpy.compareResult = false
+    const { token, err } = sut.exec('any_email', 'incorrect_password')
+    expect(token).toBeFalsy()
     expect(err).toBe('Incorrect password')
+  })
+
+  test('should return an access token if email and password are correct', () => {
+    const {
+      sut,
+      loadUserByEmailRepositorySpy,
+      accessTokenGeneratorSpy,
+      passwordHasherSpy
+    } = makeSut()
+    loadUserByEmailRepositorySpy.result = {
+      id: 'any_id',
+      createdAt: new Date(),
+      name: 'any_name',
+      email: 'any_email',
+      password: 'hashed_password'
+    }
+    passwordHasherSpy.compareResult = true
+    const { token, err } = sut.exec('correct_email', 'correct_password')
+    expect(err).toBeFalsy()
+    expect(token).toBe(accessTokenGeneratorSpy.result)
   })
 })
