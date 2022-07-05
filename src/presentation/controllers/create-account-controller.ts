5fec1350dd7416b5b4e48b23512c189bbc21e6f7
rodps <rodrigopds73@gmail.com>
2022-07-05 08:45:18 -0300

    refactor: move validation to a private method

--- src/presentation/controllers/create-account-controller.ts
+++ src/presentation/controllers/create-account-controller.ts
@@ -2,6 +2,7 @@ import { CreateAccountErrors, CreateAccountParams, CreateAccountUseCase } from '
 import { EmailAlreadyInUseError } from '../errors/email-already-in-use-error'
 import { InvalidEmailError } from '../errors/invalid-email-error'
 import { RequiredFieldsError } from '../errors/required-fields-error'
+import { ClientError } from '../protocols/client-error'
 import { HttpResponse } from '../protocols/http-response'
 import { EmailValidator } from '../validators/email-validator'
 import { RequiredFieldsValidator } from '../validators/required-fields-validator'
@@ -15,13 +16,9 @@ export class CreateAccountController {
 
   async handle (req: CreateAccountParams): Promise<HttpResponse> {
     try {
-      const requiredFields = this.requiredFieldsValidator.validate(req)
-      if (requiredFields.length > 0) {
-        return HttpResponse.badRequest(new RequiredFieldsError(requiredFields))
-      }
-      if (!this.emailValidator.isValid(req.email)) {
-        return HttpResponse.badRequest(new InvalidEmailError())
-      }
+      const clientError = this.validate(req)
+      if (clientError) return HttpResponse.badRequest(clientError)
+
       const { id, err } = await this.createAccount.exec(req)
       if (id) {
         return HttpResponse.created(`/users/${id}`)
@@ -34,6 +31,16 @@ export class CreateAccountController {
       }
     } catch {
       return HttpResponse.serverError()
+    }
+  }
+
+  private validate (req: CreateAccountParams): ClientError | undefined {
+    const requiredFields = this.requiredFieldsValidator.validate(req)
+    if (requiredFields.length > 0) {
+      return new RequiredFieldsError(requiredFields)
+    }
+    if (!this.emailValidator.isValid(req.email)) {
+      return new InvalidEmailError()
     }
   }
 }
