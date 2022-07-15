7fe7c6f8ecb498fb7735ba5be7e0474f43ff4f74
rodps <rodrigopds73@gmail.com>
2022-07-15 02:09:31 -0300

    feat: add check email repository

--- src/infra/repositories/prisma/user-repository.ts
+++ src/infra/repositories/prisma/user-repository.ts
@@ -1,8 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 import { CreateUserRepository } from '@/domain/ports/repositories/create-user-repository'
 import { CreateAccountParams } from '@/domain/ports/usecases/create-account-usecase'
+import { CheckEmailExistsRepository } from '@/domain/ports/repositories/check-email-exists-repository'
 
-export class UserRepository implements CreateUserRepository {
+export class UserRepository implements CreateUserRepository, CheckEmailExistsRepository {
   private readonly prisma: PrismaClient
   constructor (db: PrismaClient) {
     this.prisma = db
@@ -14,7 +15,13 @@ export class UserRepository implements CreateUserRepository {
         ...user
       }
     })
-
     return createdUser.id
+  }
+
+  async checkEmail (email: string): Promise<Boolean> {
+    const user = await this.prisma.user.findFirst({
+      where: { email }
+    })
+    return user !== null
   }
 }
