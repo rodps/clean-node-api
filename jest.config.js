a9b424410477e832d89fe4b5e86289d92158919a
rodps <rodrigopds73@gmail.com>
2022-07-15 06:22:29 -0300

    chore: add tests folder to jest roots

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
   collectCoverageFrom: [
     '<rootDir>/src/domain/usecases/**/*.ts',
     '<rootDir>/src/presentation/controllers/**/*.ts'
