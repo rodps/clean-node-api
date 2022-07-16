fc5778d4c27e2b1e652420a97883439499495f7a
rodps <rodrigopds73@gmail.com>
2022-07-15 23:41:31 -0300

    test: ensure RequiredFieldsValidator returns an array containing missing fields

---
+++ src/infra/validators/required-fields-validator.spec.ts
@@ -0,0 +1,8 @@
+import { RequiredFieldsValidatorImpl } from './required-fields-validator'
+
+describe('Required fields validator', () => {
+  test('should return an array containing missing parameters if the input has missing parameters', () => {
+    const sut = new RequiredFieldsValidatorImpl<any>(['field1', 'field2'])
+    expect(sut.validate({})).toEqual(['field1', 'field2'])
+  })
+})
