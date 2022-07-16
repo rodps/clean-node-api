b6ed907b88e89c6dbaf902d04fad7939a93a6933
rodps <rodrigopds73@gmail.com>
2022-07-16 02:08:53 -0300

    test: ensure RequireFieldsValidatorImpl returns missing parameters if input has empty strings

--- src/infra/validators/required-fields-validator.spec.ts
+++ src/infra/validators/required-fields-validator.spec.ts
@@ -5,4 +5,12 @@ describe('Required fields validator', () => {
     const sut = new RequiredFieldsValidatorImpl<any>(['field1', 'field2'])
     expect(sut.validate({})).toEqual(['field1', 'field2'])
   })
+
+  test('should return an array containing missing parameters if the input has empty strings', () => {
+    const sut = new RequiredFieldsValidatorImpl<any>(['field1', 'field2'])
+    expect(sut.validate({
+      field1: 0,
+      field2: ''
+    })).toEqual(['field2'])
+  })
 })
