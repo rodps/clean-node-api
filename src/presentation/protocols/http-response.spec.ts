83b620df93382f6d83b500a499bdcf8c64f7084f
rodps <rodrigopds73@gmail.com>
2022-07-19 23:54:43 -0300

    test: add http response unit tests

---
+++ src/presentation/protocols/http-response.spec.ts
@@ -0,0 +1,62 @@
+import { HttpResponse } from './http-response'
+import { ValidationError } from './validator'
+
+describe('HTTP Response', () => {
+  test('200 OK ', () => {
+    const data = 'any_data'
+    expect(HttpResponse.ok(data)).toEqual({
+      statusCode: 200,
+      body: {
+        success: true,
+        data
+      }
+    })
+  })
+
+  test('201 CREATED ', () => {
+    const data = 'any_data'
+    const location = 'any_location'
+    expect(HttpResponse.created(location, data)).toEqual({
+      statusCode: 201,
+      body: {
+        success: true,
+        data
+      },
+      header: {
+        location
+      }
+    })
+  })
+
+  test('400 BAD REQUEST', () => {
+    const errors: ValidationError[] = [
+      new ValidationError('any_field', 'any_message')
+    ]
+    expect(HttpResponse.badRequest(errors)).toEqual({
+      statusCode: 400,
+      body: {
+        success: false,
+        errors
+      }
+    })
+  })
+
+  test('409 CONFLICT', () => {
+    const errors: ValidationError[] = [
+      new ValidationError('any_field', 'any_message')
+    ]
+    expect(HttpResponse.conflict(errors)).toEqual({
+      statusCode: 409,
+      body: {
+        success: false,
+        errors
+      }
+    })
+  })
+
+  test('500 SERVER ERROR', () => {
+    expect(HttpResponse.serverError()).toEqual({
+      statusCode: 500
+    })
+  })
+})
