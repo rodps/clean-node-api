02fff5058f8aed3b3e8a948be7d5c5b3c1d995ee
rodps <rodrigopds73@gmail.com>
2022-07-20 08:51:39 -0300

    refactor: implements authorize interface

--- src/domain/usecases/authorize.ts
+++ src/domain/usecases/authorize.ts
@@ -1,29 +1,26 @@
 import { AccessTokenGenerator } from '../ports/crypt/access-token-generator'
 import { PasswordHasher } from '../ports/crypt/password-hasher'
 import { LoadUserByEmailRepository } from '../ports/repositories/load-user-by-email-repository'
+import { AccessTokenOrError, AuthorizeErrors, AuthorizeParams, AuthorizeUseCase } from '../ports/usecases/authorize-usecase'
 
-interface AccessTokenOrError {
-  token?: string
-  err?: string
-}
-
-export class Authorize {
+export class Authorize implements AuthorizeUseCase {
   constructor (
     private readonly loadUserByEmailRepository: LoadUserByEmailRepository,
     private readonly accessTokenGenerator: AccessTokenGenerator,
     private readonly passwordHasher: PasswordHasher
   ) {}
 
-  async exec (email: string, password: string): Promise<AccessTokenOrError> {
+  async exec (params: AuthorizeParams): Promise<AccessTokenOrError> {
+    const { email, password } = params
     const user = await this.loadUserByEmailRepository.load(email)
     if (!user) {
-      return { err: 'Email not registered' }
+      return { err: AuthorizeErrors.EMAIL_NOT_REGISTERED }
     }
     if (!await this.passwordHasher.compare(password, user.password)) {
-      return { err: 'Incorrect password' }
+      return { err: AuthorizeErrors.INCORRECT_PASSWORD }
     }
     return {
-      token: this.accessTokenGenerator.generate({ id: user.id, userName: user.name })
+      accessToken: this.accessTokenGenerator.generate({ id: user.id, userName: user.name })
     }
   }
 }
