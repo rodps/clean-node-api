438fd7bc24e68c6f1f789c82c7c66811c1afc573
rodps <rodrigopds73@gmail.com>
2022-07-21 15:19:55 -0300

    feat: add /signin route

--- src/infra/http/express/routes/auth-routes.ts
+++ src/infra/http/express/routes/auth-routes.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
+import makeAuthenticate from '@/main/factories/authenticate-factory'
 import { makeCreateAccount } from '@/main/factories/create-account-factory'
 import { Router, Request, Response } from 'express'
 const router = Router()
@@ -6,7 +7,19 @@ const router = Router()
 router.post('/signup', async (req: Request, res: Response) => {
   const createAccountController = makeCreateAccount()
   const httpResponse = await createAccountController.handle(req.body)
-  res.status(httpResponse.statusCode).header(httpResponse.header).send(httpResponse.body)
+  res
+    .status(httpResponse.statusCode)
+    .header(httpResponse.header)
+    .send(httpResponse.body)
+})
+
+router.post('/signin', async (req: Request, res: Response) => {
+  const authenticateController = makeAuthenticate()
+  const httpResponse = await authenticateController.handle(req.body)
+  res
+    .status(httpResponse.statusCode)
+    .header(httpResponse.header)
+    .send(httpResponse.body)
 })
 
 export default router
