3f556e8577e842313d5057b15a47c5d63a30e670
rodps <rodrigopds73@gmail.com>
2022-07-21 12:05:05 -0300

    feat: return unauthorized if authenticate returns EMAIL_NOT_REGISTERED

--- src/presentation/controllers/authenticate-controller.ts
+++ src/presentation/controllers/authenticate-controller.ts
@@ -1,4 +1,5 @@
-import { AuthenticateParams, AuthenticateUseCase } from '@/domain/ports/usecases/authenticate-usecase'
+import { AuthenticateErrors, AuthenticateParams, AuthenticateUseCase } from '@/domain/ports/usecases/authenticate-usecase'
+import { EmailNotRegisteredError } from '../errors/email-not-registered-error'
 import { HttpResponse } from '../protocols/http-response'
 import { Validator } from '../protocols/validator'
 
@@ -11,6 +12,17 @@ export class AuthenticateController {
   async handle (params: AuthenticateParams): Promise<HttpResponse> {
     const validationError = this.validator.validate(params)
     if (validationError) return HttpResponse.badRequest(validationError)
-    return HttpResponse.ok()
+
+    const { accessToken, err } = await this.authenticate.exec(params)
+    if (accessToken) {
+      return HttpResponse.ok()
+    } else {
+      switch (err) {
+        case AuthenticateErrors.EMAIL_NOT_REGISTERED:
+          return HttpResponse.unauthorized([new EmailNotRegisteredError('email')])
+        default:
+          return HttpResponse.serverError()
+      }
+    }
   }
 }
