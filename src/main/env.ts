c873c2d6d54dee2c1563eb6e5e829e8ae329d3d8
rodps <rodrigopds73@gmail.com>
2022-07-21 15:08:08 -0300

    refactor: add env defaults

--- src/main/env.ts
+++ src/main/env.ts
@@ -3,3 +3,10 @@ import path from 'path'
 const envFile = `.env.${process.env.NODE_ENV ?? 'dev'}`
 const envPath = path.resolve(process.cwd(), envFile)
 dotenv.config({ path: envPath })
+
+const port = 3000
+export const env = {
+  port: process.env.APP_PORT ?? port,
+  baseUrl: process.env.BASE_URL ?? `http://localhost:${port}`,
+  jwtSecret: process.env.JWT_SECRET ?? 'secret'
+}
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,6 @@
 import app from './config'
+import { env } from './env'
 
 app.listen(process.env.APP_PORT, () => {
-  console.log(`server listen on port: ${process.env.APP_PORT ?? ''}`)
+  console.log(`server listen on port: ${env.port}`)
 })
