6f5675a1c8f4fbe66be9d57dd18712df646ec16d
rodps <rodrigopds73@gmail.com>
2022-07-21 13:55:17 -0300

    feat: return unauthorized if authentication returns INCORRECT_PASSWORD

--- src/presentation/controllers/authenticate-controller.ts
+++ src/presentation/controllers/authenticate-controller.ts
@@ -1,5 +1,6 @@
 import { AuthenticateErrors, AuthenticateParams, AuthenticateUseCase } from '@/domain/ports/usecases/authenticate-usecase'
 import { EmailNotRegisteredError } from '../errors/email-not-registered-error'
+import { IncorrectPasswordError } from '../errors/incorrect-password-error'
 import { HttpResponse } from '../protocols/http-response'
 import { Validator } from '../protocols/validator'
 
@@ -20,6 +21,8 @@ export class AuthenticateController {
       switch (err) {
         case AuthenticateErrors.EMAIL_NOT_REGISTERED:
           return HttpResponse.unauthorized([new EmailNotRegisteredError('email')])
+        case AuthenticateErrors.INCORRECT_PASSWORD:
+          return HttpResponse.unauthorized([new IncorrectPasswordError('password')])
         default:
           return HttpResponse.serverError()
       }
