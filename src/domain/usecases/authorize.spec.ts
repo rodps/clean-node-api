22b0644de26d0d67644b7ac9aaf50d2ddf831d29
rodps <rodrigopds73@gmail.com>
2022-07-21 19:29:46 -0300

    test: ensure Authorize returns null if TokenVerifier returns null

--- src/domain/usecases/authorize.spec.ts
+++ src/domain/usecases/authorize.spec.ts
@@ -16,11 +16,31 @@ class TokenVerifierSpy implements TokenVerifier {
   }
 }
 
+interface SutTypes {
+  tokenVerifierSpy: TokenVerifierSpy
+  sut: Authorize
+}
+
+const makeSut = (): SutTypes => {
+  const tokenVerifierSpy = new TokenVerifierSpy()
+  const sut = new Authorize(tokenVerifierSpy, 'any_role')
+  return {
+    tokenVerifierSpy,
+    sut
+  }
+}
+
 describe('Authorize use case', () => {
   test('should call TokenVerifier with correct values', async () => {
-    const tokenVerifierSpy = new TokenVerifierSpy()
-    const sut = new Authorize(tokenVerifierSpy, 'any_role')
+    const { tokenVerifierSpy, sut } = makeSut()
     await sut.exec('any_token')
     expect(tokenVerifierSpy.token).toBe('any_token')
+  })
+
+  test('should return null if TokenVerifier returns null', async () => {
+    const { tokenVerifierSpy, sut } = makeSut()
+    tokenVerifierSpy.result = null
+    const payload = await sut.exec('any_token')
+    expect(payload).toBe(null)
   })
 })
