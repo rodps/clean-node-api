a0c6ea38c6389ec2e370944c4658ebc688d714dd
rodps <rodrigopds73@gmail.com>
2022-07-21 15:06:29 -0300

    test: ensure /signup returns 409

--- tests/integration/infra/http/auth-routes.test.ts
+++ tests/integration/infra/http/auth-routes.test.ts
@@ -1,30 +1,51 @@
 import app from '@/main/config'
 import prisma from '@/infra/repositories/prisma/client'
 import request from 'supertest'
+import { env } from '@/main/env'
 
 describe('Auth routes test', () => {
   afterEach(async () => {
     await prisma.user.deleteMany()
   })
 
-  test('POST /signup', async () => {
-    const response = await request(app)
-      .post('/signup')
-      .send({
-        name: 'Rodrigo',
-        email: 'rodrigo@gmail.com',
-        password: '123456789'
-      })
+  describe('POST /signup', () => {
+    test('should return 201', async () => {
+      const response = await request(app)
+        .post('/signup')
+        .send({
+          name: 'Rodrigo',
+          email: 'rodrigo@gmail.com',
+          password: '123456789'
+        })
 
-    const user = await prisma.user.findUnique({
-      where: {
-        email: 'rodrigo@gmail.com'
+      const user = await prisma.user.findUnique({
+        where: {
+          email: 'rodrigo@gmail.com'
+        }
+      })
+      expect(user).toBeTruthy()
+      if (user) {
+        expect(response.status).toEqual(201)
+        expect(response.header).toHaveProperty('location', `${env.baseUrl}/users/${user.id}`)
       }
     })
-    expect(user).toBeTruthy()
-    if (user) {
-      expect(response.status).toEqual(201)
-      expect(response.header).toHaveProperty('location', `${process.env.BASE_URL ?? ''}/users/${user.id}`)
-    }
+
+    test('should return 409', async () => {
+      await prisma.user.create({
+        data: {
+          name: 'Rodrigo',
+          email: 'rodrigo@gmail.com',
+          password: '12345789'
+        }
+      })
+      await request(app)
+        .post('/signup')
+        .send({
+          name: 'Rodrigo',
+          email: 'rodrigo@gmail.com',
+          password: '123456789'
+        })
+        .expect(409)
+    })
   })
 })
