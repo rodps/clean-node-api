514e5873c1ddcd97dd3b4ce2ae9157b4ad9c5fc8
rodps <rodrigopds73@gmail.com>
2022-07-24 15:06:02 -0300

    test: ensure POST /books returns 201

---
+++ tests/integration/infra/http/books-routes.test.ts
@@ -0,0 +1,30 @@
+import prisma from '@/infra/repositories/prisma/client'
+import app from '@/main/config'
+import { env } from '@/main/env'
+import request from 'supertest'
+
+describe('Books routes test', () => {
+  afterEach(async () => {
+    await prisma.user.deleteMany()
+  })
+  describe('POST /books', () => {
+    test('should return 201', async () => {
+      const response = await request(app)
+        .post('/books')
+        .send({
+          title: 'any_title',
+          isbn: 'any_isbn',
+          pages: 999,
+          author: 'any_author',
+          publish_date: 'any_date',
+          genre: 'any_genre',
+          edition: 1,
+          publisher: 'any_publisher',
+          description: 'any_description',
+          copies: 1
+        })
+        .expect(201)
+      expect(response.headers.location).toBe(`${env.baseUrl}/books/${response.body.data.id as string}`)
+    })
+  })
+})
