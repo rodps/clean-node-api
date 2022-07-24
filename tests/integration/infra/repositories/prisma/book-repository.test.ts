2f0ed10cf5fbcec65b66af5496240784c498b3c4
rodps <rodrigopds73@gmail.com>
2022-07-24 10:45:35 -0300

    test: ensure BookRepository checks if isbn has already been added

--- tests/integration/infra/repositories/prisma/book-repository.test.ts
+++ tests/integration/infra/repositories/prisma/book-repository.test.ts
@@ -1,3 +1,4 @@
+import { BookRepository } from '@/infra/repositories/prisma/book-repository'
 import prisma from './client'
 
 describe('Book repository', () => {
@@ -17,9 +18,8 @@ describe('Book repository', () => {
       publisher: 'any_publisher',
       title: 'any_title'
     }
-    const createdBook = await prisma.book.create({
-      data: book
-    })
+    const sut = new BookRepository(prisma)
+    const createdBook = await sut.add(book)
 
     const bookDb = await prisma.book.findUnique({
       where: {
@@ -32,5 +32,25 @@ describe('Book repository', () => {
       const { id, createdAt, updatedAt, ...bookWithoutIdAndDates } = bookDb
       expect(bookWithoutIdAndDates).toEqual(book)
     }
+  })
+
+  test('should check if isbn has already been added', async () => {
+    const book = {
+      author: 'any_author',
+      copies: 1,
+      description: 'any_description',
+      edition: 1,
+      genre: 'any_genre',
+      isbn: 'any_isbn',
+      pages: 1,
+      publish_date: 'any_date',
+      publisher: 'any_publisher',
+      title: 'any_title'
+    }
+    const sut = new BookRepository(prisma)
+    await sut.add(book)
+    await expect(sut.checkIsbn(book.isbn)).resolves.toBe(true)
+    await prisma.book.deleteMany()
+    await expect(sut.checkIsbn(book.isbn)).resolves.toBe(false)
   })
 })
