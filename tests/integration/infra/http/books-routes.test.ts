e70a0871dd37e990761bce0843ac10cabfc0e0b1
rodps <rodrigopds73@gmail.com>
2022-07-24 15:17:55 -0300

    test: ensure POST /books returns 409

--- tests/integration/infra/http/books-routes.test.ts
+++ tests/integration/infra/http/books-routes.test.ts
@@ -5,7 +5,7 @@ import request from 'supertest'
 
 describe('Books routes test', () => {
   afterEach(async () => {
-    await prisma.user.deleteMany()
+    await prisma.book.deleteMany()
   })
   describe('POST /books', () => {
     test('should return 201', async () => {
@@ -25,6 +25,28 @@ describe('Books routes test', () => {
         })
         .expect(201)
       expect(response.headers.location).toBe(`${env.baseUrl}/books/${response.body.data.id as string}`)
+    })
+
+    test('should return 409', async () => {
+      const book = {
+        title: 'any_title',
+        isbn: 'any_isbn',
+        pages: 999,
+        author: 'any_author',
+        publish_date: 'any_date',
+        genre: 'any_genre',
+        edition: 1,
+        publisher: 'any_publisher',
+        description: 'any_description',
+        copies: 1
+      }
+      await prisma.book.create({
+        data: book
+      })
+      await request(app)
+        .post('/books')
+        .send(book)
+        .expect(409)
     })
   })
 })
