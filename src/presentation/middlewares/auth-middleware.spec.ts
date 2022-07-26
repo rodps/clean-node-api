90a89183005afb49bb0816150d38053193c92694
rodps <rodrigopds73@gmail.com>
2022-07-26 09:50:36 -0300

    test: ensure AuthMiddleware returns id if Authorize succeed

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,15 +1,36 @@
 import { AuthMiddleware } from './auth-middleware'
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 import { AuthorizeUseCase } from '@/domain/ports/usecases/authorize-usecase'
 import { HttpResponse } from '../protocols/http-response'
 
+interface SutTypes {
+  authorizeSpy: MockProxy<AuthorizeUseCase>
+  sut: AuthMiddleware
+}
+
+const makeSut = (): SutTypes => {
+  const authorizeSpy = mock<AuthorizeUseCase>()
+  const sut = new AuthMiddleware(authorizeSpy)
+  return {
+    authorizeSpy,
+    sut
+  }
+}
+
 describe('Auth middleware', () => {
   test('should return unauthorized error if Authorize fails', async () => {
-    const authorizeSpy = mock<AuthorizeUseCase>()
+    const { authorizeSpy, sut } = makeSut()
     authorizeSpy.exec.mockResolvedValue(null)
-    const sut = new AuthMiddleware(authorizeSpy)
     const result = await sut.handle('any_token')
     expect(result.error).toEqual(HttpResponse.unauthorized())
     expect(result.id).toBeFalsy()
+  })
+
+  test('should return id if Authorize succeed', async () => {
+    const { authorizeSpy, sut } = makeSut()
+    authorizeSpy.exec.mockResolvedValue('any_id')
+    const result = await sut.handle('any_token')
+    expect(result.error).toBeFalsy()
+    expect(result.id).toBe('any_id')
   })
 })
