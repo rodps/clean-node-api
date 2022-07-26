1fab40fb6169bc1e63c72da050c9e4ec18b8e626
rodps <rodrigopds73@gmail.com>
2022-07-26 10:42:13 -0300

    feat: add JWTAdapter verify

--- src/infra/cryptography/jsonwebtoken-adapter.ts
+++ src/infra/cryptography/jsonwebtoken-adapter.ts
@@ -1,9 +1,19 @@
 import { AccessTokenGenerator, AccessTokenPayload } from '@/domain/ports/crypt/access-token-generator'
+import { TokenVerifier } from '@/domain/ports/crypt/token-verifier'
 import jwt from 'jsonwebtoken'
 
-export class JWTAdapter implements AccessTokenGenerator {
+export class JWTAdapter implements AccessTokenGenerator, TokenVerifier {
   constructor (private readonly secret: string) {}
   generate (payload: AccessTokenPayload): string {
     return jwt.sign(payload, this.secret)
+  }
+
+  async verify (token: string): Promise<AccessTokenPayload | null> {
+    try {
+      const payload = jwt.verify(token, this.secret) as jwt.JwtPayload & AccessTokenPayload
+      return { id: payload.id, role: payload.role }
+    } catch {
+      return null
+    }
   }
 }
