904e65090a1282d1cdb857b0f3db0c8da63ffdc0
rodps <rodrigopds73@gmail.com>
2022-07-27 18:51:12 -0300

    refactor: change .env loading

--- src/main/env.ts
+++ src/main/env.ts
@@ -1,8 +1,12 @@
 import dotenv from 'dotenv'
 import path from 'path'
-const envFile = `.env.${process.env.NODE_ENV ?? 'dev'}`
-const envPath = path.resolve(process.cwd(), envFile)
-dotenv.config({ path: envPath })
+if (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'test') {
+  const envFile = `.env.${process.env.NODE_ENV}`
+  const envPath = path.resolve(process.cwd(), envFile)
+  dotenv.config({ path: envPath })
+} else {
+  dotenv.config()
+}
 
 const port = 3000
 export const env = {
